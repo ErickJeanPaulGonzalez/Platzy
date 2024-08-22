12ae4f4e07437f9ee443185ad830233eab5cf191
Erick Gonzalez <erickjeanpaulgonzalez@outlook.com>
2024-08-22 00:36:16 -0500

    Avanced

---
+++ github-tracker/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"github.com/gorilla/mux")
+
+func postHandler(w http.ResponseWriter, r *http.Request ){
+	fmt.Println("received POST resquest")
+
+	defer r.Body.Close()
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil{
+		fmt.Println("error reading the request")
+		
+		return
+	}
+	fmt.Printf(string(body))
+}
+
+func main(){
+	router := mux.NewRouter()
+
+	router.HandleFunc("/hello", postHandler).Methods("POST")
+
+	fmt.Println("server listen on port 8080")
+	err := http.ListenAndServe(":8080", router)
+	if err != nil{
+		fmt.Println(err.Error())
+	}
+}
