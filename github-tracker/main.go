79f2cc5540549d1c6f946aca550ad929d7a67573
Erick Gonzalez <erickjeanpaulgonzalez@outlook.com>
2024-08-22 00:41:44 -0500

    Avanced 2

--- github-tracker/main.go
+++ github-tracker/main.go
@@ -4,30 +4,33 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"github.com/gorilla/mux")
 
-func postHandler(w http.ResponseWriter, r *http.Request ){
-	fmt.Println("received POST resquest")
+	"github.com/gorilla/mux"
+)
+
+func postHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Received POST request")
 
 	defer r.Body.Close()
 
 	body, err := io.ReadAll(r.Body)
-	if err != nil{
-		fmt.Println("error reading the request")
-		
+	if err != nil {
+		fmt.Println("Error reading the request body:", err)
+		http.Error(w, "Can't read the request body", http.StatusBadRequest)
 		return
 	}
-	fmt.Printf(string(body))
+	
+	fmt.Println(string(body))  // Use Println instead of Printf
 }
 
-func main(){
+func main() {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/hello", postHandler).Methods("POST")
 
-	fmt.Println("server listen on port 8080")
+	fmt.Println("Server listening on port 8080")
 	err := http.ListenAndServe(":8080", router)
-	if err != nil{
-		fmt.Println(err.Error())
+	if err != nil {
+		fmt.Println("Server error:", err.Error())
 	}
 }
